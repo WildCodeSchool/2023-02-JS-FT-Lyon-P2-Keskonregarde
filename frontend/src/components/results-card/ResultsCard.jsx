6d2b11b6bf92fec62390afeab683107afaa9904c
LightQv <QVivian23@gmail.com>
2023-04-20 17:04:33 +0200

    Added Link to MovieCard from ResultsCard

--- frontend/src/components/results-card/ResultsCard.jsx
+++ frontend/src/components/results-card/ResultsCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./ResultsCard.module.css";
@@ -9,26 +10,28 @@ export default function ResultsCard({ movies, page, setPage }) {
     <div className={styles.searchResults}>
       <div className={styles.searchCard}>
         {movies.map((movie) => (
-          <div key={movie.id} className={styles.cardContainer}>
-            <div className={styles.posterContainer}>
-              <img
-                src={`${posterUrl}${movie.poster_path}`}
-                alt={movie.orginal_title}
-                className={styles.poster}
-              />
-            </div>
-            <div className={styles.infoContainer}>
-              <div>
-                <h4 className={styles.movieTitle}>{movie.title}</h4>
-                <h6 className={styles.movieDate}>
-                  {movie.release_date.slice(0, 4)}
-                </h6>
+          <Link to="/movie/:id">
+            <div key={movie.id} className={styles.cardContainer}>
+              <div className={styles.posterContainer}>
+                <img
+                  src={`${posterUrl}${movie.poster_path}`}
+                  alt={movie.orginal_title}
+                  className={styles.poster}
+                />
               </div>
-              <p className={styles.movieSynopsis}>
-                {`${movie.overview.slice(0, 75)}...`}
-              </p>
+              <div className={styles.infoContainer}>
+                <div>
+                  <h4 className={styles.movieTitle}>{movie.title}</h4>
+                  <h6 className={styles.movieDate}>
+                    {movie.release_date.slice(0, 4)}
+                  </h6>
+                </div>
+                <p className={styles.movieSynopsis}>
+                  {`${movie.overview.slice(0, 75)}...`}
+                </p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
       <div className={styles.buttonContainer}>
