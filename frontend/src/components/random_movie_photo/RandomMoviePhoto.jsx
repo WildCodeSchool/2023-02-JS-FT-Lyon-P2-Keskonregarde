bb7e5ba1e709f095c7ef607a5a3c56181b482ab5
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-04-21 11:40:56 +0200

    Random movie Photo done with css

--- frontend/src/components/random_movie_photo/RandomMoviePhoto.jsx
+++ frontend/src/components/random_movie_photo/RandomMoviePhoto.jsx
@@ -5,16 +5,29 @@ import styles from "./RandomMoviePhoto.module.css";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 export default function RandomMoviePhoto() {
-  const [randomMovies, setRandomMovies] = useState("");
+  const [movies, setMovies] = useState("");
 
   const url = "https://image.tmdb.org/t/p/original";
+  const randomMovies = [
+    `https://api.themoviedb.org/3/movie/640146?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/438631?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/157336?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/19995?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/862?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/680?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/129?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/8587?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/700391?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/458156?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/278?api_key=${API_KEY}&language=fr&include_adult=false`,
+    `https://api.themoviedb.org/3/movie/274?api_key=${API_KEY}&language=fr&include_adult=false`,
+  ];
   useEffect(() => {
     axios
-      .get(
-        `https://api.themoviedb.org/3/movie/438631?api_key=${API_KEY}&language=fr&include_adult=false`
-      )
-      .then(({ data }) => {
-        setRandomMovies(data);
+      .all(randomMovies.map((randomPhoto) => axios.get(randomPhoto)))
+      .then((data) => {
+        const randomPhoto = Math.floor(Math.random() * 12);
+        setMovies(data[randomPhoto].data);
       });
   }, []);
 
@@ -22,7 +35,7 @@ export default function RandomMoviePhoto() {
     <div
       className={styles.moviePhoto}
       style={{
-        backgroundImage: `url(${url}${randomMovies.backdrop_path})`,
+        backgroundImage: `url(${url}${movies.backdrop_path})`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "top",
         backgroundSize: "cover",
@@ -30,10 +43,8 @@ export default function RandomMoviePhoto() {
     >
       <div>
         <div>
-          <h2 className={styles.titleMoviePhoto}>
-            {randomMovies.original_title}
-          </h2>
-          <h3 className={styles.dateMoviePhoto}>{randomMovies.release_date}</h3>
+          <h2 className={styles.titleMoviePhoto}>{movies.title}</h2>
+          <h3 className={styles.dateMoviePhoto}>{movies.release_date}</h3>
         </div>
       </div>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,8 @@
-import axios from "axios";
 import "../App.css";
-import { useEffect, useState } from "react";
 import SearchBar from "../components/search-bar/SearchBar";
 import RandomMoviePhoto from "../components/random_movie_photo/RandomMoviePhoto";
 
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-
 export default function Home() {
-  const [movies, setMovies] = useState(null);
-  const [query, setQuery] = useState("");
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=fr&query=${query}&page=1&include_adult=false&limit=1`
-      )
-      .then(({ data }) => {
-        setMovies(data.results);
-      });
-  }, [query]);
-
-  if (!movies) return null;
   return (
     <div>
       <header>
@@ -29,7 +11,7 @@ export default function Home() {
           alt="logo"
           className="logo-pic"
         />
-        <SearchBar setQuery={setQuery} />
+        <SearchBar />
       </header>
       <RandomMoviePhoto />
     </div>
