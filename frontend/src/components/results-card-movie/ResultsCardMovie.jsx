094786b99d71406c34e5aeb92af9d4581ab1c1b8
LightQv <QVivian23@gmail.com>
2023-04-28 17:06:08 +0200

    "results card tv done + button to switch request"

---
+++ frontend/src/components/results-card-movie/ResultsCardMovie.jsx
@@ -0,0 +1,99 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import PropTypes from "prop-types";
+import styles from "./ResultsCardMovie.module.css";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+export default function ResultsCardMovie({
+  movies,
+  setMovies,
+  pageNumber,
+  setPageNumber,
+}) {
+  const [results] = useSearchParams();
+  const [hasMore, setHasMore] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchMoreData();
+  }, [pageNumber]);
+
+  const fetchMoreData = () => {
+    const query = results.get("query") || "";
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=fr&query=${query}&page=${pageNumber}&include_adult=false`
+      )
+      .then(({ data }) => {
+        if (data.page !== data.total_pages) {
+          setMovies([...movies, ...data.results]);
+        } else setHasMore(false);
+      })
+      .catch((err) =>
+        err.response.status === 404 ? navigate("/not-found") : null
+      );
+  };
+
+  const posterUrl = "https://image.tmdb.org/t/p/w200";
+
+  return (
+    <div className={styles.infiniteScrollBox} id="scroll-box">
+      <InfiniteScroll
+        dataLength={movies.length}
+        next={() => setPageNumber(pageNumber + 1)}
+        hasMore={hasMore}
+        loader={<p className="results-number">Chargement...</p>}
+        endMessage={
+          <p className="results-number">Il n'y a pas plus de résultats.</p>
+        }
+        scrollableTarget="scroll-box"
+      >
+        <div className={styles.searchResults}>
+          <div className={styles.searchCard}>
+            {movies.map((movie) => (
+              <Link key={movie.id} to={`/movie/${movie.id}`}>
+                <div className={styles.cardContainer}>
+                  <div className={styles.posterContainer}>
+                    <img
+                      src={
+                        movie.poster_path
+                          ? `${posterUrl}${movie.poster_path}`
+                          : null
+                      }
+                      alt={movie.orginal_title}
+                      className={styles.poster}
+                    />
+                  </div>
+                  <div className={styles.infoContainer}>
+                    <div>
+                      <h4 className={styles.movieTitle}>{movie.title}</h4>
+                      <h6 className={styles.movieDate}>
+                        {movie.release_date
+                          ? movie.release_date.slice(0, 4)
+                          : null}
+                      </h6>
+                    </div>
+                    <p className={styles.movieSynopsis}>
+                      {`${movie.overview.slice(0, 75)}...`}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+}
+
+ResultsCardMovie.propTypes = {
+  movies: PropTypes.shape().isRequired,
+  setMovies: PropTypes.func.isRequired,
+  pageNumber: PropTypes.number.isRequired,
+  setPageNumber: PropTypes.func.isRequired,
+};
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,45 +1,88 @@
 import axios from "axios";
 import "../App.css";
 import { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import ResultsCard from "../components/results-card/ResultsCard";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
+import ResultsCardMovie from "../components/results-card-movie/ResultsCardMovie";
+import ResultsCardTv from "../components/results-card-tv/ResultsCardTv";
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 export default function Search() {
   const [results] = useSearchParams();
 
+  const [requestType, setRequestType] = useState("movie");
   const [requestedData, setRequestedData] = useState(null);
   const [movies, setMovies] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
+  console.log(requestType);
 
+  const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
     const query = results.get("query") || "";
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=fr&query=${query}&page=${pageNumber}&include_adult=false`
-      )
-      .then(({ data }) => {
-        if (data.total_results > 0) {
-          setRequestedData(data);
-          setMovies(data.results);
-        } else navigate("/search/no-results");
-      })
-      .catch((err) =>
-        err.response.status === 404 ? navigate("/not-found") : null
-      );
-  }, [results, pageNumber]);
+    if (pageNumber === 1)
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/${requestType}?api_key=${API_KEY}&language=fr&query=${query}&page=${pageNumber}&include_adult=false`
+        )
+        .then(({ data }) => {
+          if (data.total_results > 0) {
+            console.log("then");
+            setRequestedData(data);
+            setMovies(data.results);
+          } else navigate("/search/no-results");
+        })
+        .catch((err) =>
+          err.response.status === 404 ? navigate("/not-found") : null
+        );
+    return () => {};
+  }, [results, pageNumber, location, location.search, location.state]);
 
-  if (!requestedData) return null;
+  if (!requestedData || !requestType) return null;
   return (
     <>
+      <div className="switch-type-container">
+        <button
+          type="button"
+          className={
+            requestType === "movie"
+              ? "button-select-type-enabled"
+              : "button-select-type"
+          }
+          onClick={() => {
+            setRequestType("movie");
+          }}
+        >
+          Films
+        </button>
+        <button
+          type="button"
+          className={
+            requestType === "tv"
+              ? "button-select-type-enabled"
+              : "button-select-type"
+          }
+          onClick={() => {
+            setRequestType("tv");
+          }}
+        >
+          Séries TV
+        </button>
+      </div>
       <h5 className="results-number">
         Résultats trouvés : {requestedData.total_results}
       </h5>
-      {movies && (
-        <ResultsCard
+      {requestType === "movie" && movies && (
+        <ResultsCardMovie
+          movies={movies}
+          setMovies={setMovies}
+          pageNumber={pageNumber}
+          setPageNumber={setPageNumber}
+        />
+      )}
+      {requestType === "tv" && movies && (
+        <ResultsCardTv
           movies={movies}
           setMovies={setMovies}
           pageNumber={pageNumber}
